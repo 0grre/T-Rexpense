eeea9e8722642e0bd9864fd70d50457992761434
jb loup <jb@wokine.com>
2022-08-11 15:52:38 +0200

    feat(rgpd): add privacy and terms pages

--- resources/views/components/terms/en.blade.php
+++ resources/views/components/terms/en.blade.php
@@ -1,58 +1,136 @@
 <h1 id="terms" class="card-title">Terms</h1>
 <br>
 <br>
-<h2 id="identity">IDENTITÉ</h2>
-<p><strong>Nom du site web</strong>: <em>T-Rexpense</em></p>
-<p><strong>Adresse</strong>: <em><a href="https://t-rexpense.xyz">https://t-rexpense.xyz</a></em></p>
-<p><strong>Propriétaire</strong>: <em>Jean-Baptiste Loup, Lille - France</em></p>
-<p><strong>Conception et réalisation</strong>: <em>Jean-Baptiste Loup, Lille - France</em></p>
-<p><strong>Hébergement</strong>: <em>IONOS SARL (7, place de la Gare, BP 70109, 57200 Sarreguemines Cedex, 431 303 775
-        RCS Sarreguemines, tél. : 0970 808 911, info@IONOS.fr)</em></p>
+<h2><strong>Terms and Conditions</strong></h2>
 <br>
-<h2 id="terms-of-use">TERMS OF USE</h2>
-<p>The use of this site implies full acceptance of the general conditions of use described below. These terms of use may
-    be
-    modified or supplemented at any time.</p>
+
+<p>Welcome to T-Rexpense!</p>
+
+<p>These terms and conditions outline the rules and regulations for the use of T-Rexpense's Website, located at t-rexpense.xyz.</p>
+
+<p>By accessing this website we assume you accept these terms and conditions. Do not continue to use T-Rexpense if you do not agree to take all of the terms and conditions stated on this page.</p>
+
+<p>The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to same.</p>
+
+<h3><strong>Cookies</strong></h3>
 <br>
-<h2 id="informations">INFORMATIONS</h2>
-<p>The information and documents of the site are presented as an indication, without exhaustive nature, and can not
-    engage
-    the responsibility of the owner of the site. The owner of the site can not be held responsible for direct and
-    indirect
-    damage resulting from access to the site.</p>
+
+<p>We employ the use of cookies. By accessing T-Rexpense, you agreed to use cookies in agreement with the T-Rexpense's Privacy Policy. </p>
+
+<p>Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.</p>
+
+<h3><strong>License</strong></h3>
 <br>
-<h2 id="interactivity">INTERACTIVITY</h2>
-<p>Users of the site can deposit content, appearing on the site in dedicated spaces (in particular via comments). The
-    deposited content remains under the responsibility of their authors, who fully assume the entirety legal liability.
-    The
-    owner of the site nevertheless reserves the right to withdraw without notice and without justification any content
-    posted by users who do not comply with the code of ethics of the site or the legislation in force.</p>
+
+<p>Unless otherwise stated, T-Rexpense and/or its licensors own the intellectual property rights for all material on T-Rexpense. All intellectual property rights are reserved. You may access this from T-Rexpense for your own personal use subjected to restrictions set in these terms and conditions.</p>
+
+<p>You must not:</p>
+<ul>
+    <li>Republish material from T-Rexpense</li>
+    <li>Sell, rent or sub-license material from T-Rexpense</li>
+    <li>Reproduce, duplicate or copy material from T-Rexpense</li>
+    <li>Redistribute content from T-Rexpense</li>
+</ul>
+
+<p>This Agreement shall begin on the date hereof. Our Terms and Conditions were created with the help of the <a href="https://www.termsandconditionsgenerator.com/">Free Terms and Conditions Generator</a>.</p>
+
+<p>Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. T-Rexpense does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of T-Rexpense,its agents and/or affiliates. Comments reflect the views and opinions of the person who post their views and opinions. To the extent permitted by applicable laws, T-Rexpense shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.</p>
+
+<p>T-Rexpense reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.</p>
+
+<p>You warrant and represent that:</p>
+
+<ul>
+    <li>You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+    <li>The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+    <li>The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy</li>
+    <li>The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+</ul>
+
+<p>You hereby grant T-Rexpense a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.</p>
+
+<h3><strong>Hyperlinking to our Content</strong></h3>
 <br>
-<h2 id="intellectual-property">INTELLECTUAL PROPERTY</h2>
-<p>Unless otherwise stated, all elements accessible on the site (texts, images, graphics, logo, icons, sounds, software,
-    etc.) remain the exclusive property of their authors, with regard to property rights intellectual or rights of use.
-    1
-    Any reproduction, representation, modification, publication, adaptation of all or part of the elements of the site,
-    what
-    whether the means or process used, is prohibited without the prior written permission of the author.23 Any
-    unauthorized
-    use of the site or any of the elements it contains is considered to be constituting an infringement and liable to
-    prosecution. 4 The trademarks and logos reproduced on the site are registered by the companies that own them.</p>
+
+<p>The following organizations may link to our Website without prior written approval:</p>
+
+<ul>
+    <li>Government agencies;</li>
+    <li>Search engines;</li>
+    <li>News organizations;</li>
+    <li>Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
+    <li>System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
+</ul>
+
+<p>These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.</p>
+
+<p>We may consider and approve other link requests from the following types of organizations:</p>
+
+<ul>
+    <li>commonly-known consumer and/or business information sources;</li>
+    <li>dot.com community sites;</li>
+    <li>associations or other groups representing charities;</li>
+    <li>online directory distributors;</li>
+    <li>internet portals;</li>
+    <li>accounting, law and consulting firms; and</li>
+    <li>educational institutions and trade associations.</li>
+</ul>
+
+<p>We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of T-Rexpense; and (d) the link is in the context of general resource information.</p>
+
+<p>These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party’s site.</p>
+
+<p>If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to T-Rexpense. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Wait 2-3 weeks for a response.</p>
+
+<p>Approved organizations may hyperlink to our Website as follows:</p>
+
+<ul>
+    <li>By use of our corporate name; or</li>
+    <li>By use of the uniform resource locator being linked to; or</li>
+    <li>By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</li>
+</ul>
+
+<p>No use of T-Rexpense's logo or other artwork will be allowed for linking absent a trademark license agreement.</p>
+
+<h3><strong>iFrames</strong></h3>
 <br>
-<h2 id="link">LINK</h2>
-<h3 id="outbound-links">Outbound links</h3>
-<p>The owner of the site declines all responsibility and is not engaged by the referencing via hypertext links,
-    third-party
-    resources on the Internet, both in terms of their content and relevance.</p>
-<h3 id="inbound-links">Inbound links</h3>
-<p>The owner of the site authorizes hypertext links to one of the pages of this site, provided that they open a new
-    window
-    and are presented unequivocally to avoid: any risk of confusion between the citing site and the owner of the site as
-    well as any biased presentation, or contrary to the laws in force. The site owner reserves the right to request the
-    removal of a link if it considers that the source site does not does not comply with the rules thus defined.</p>
+
+<p>Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.</p>
+
+<h3><strong>Content Liability</strong></h3>
 <br>
-<h2 id="confidentiality">CONFIDENTIALITY</h2>
-<p>Any user has a right of access, rectification and opposition to personal data concerning him, in making his written
-    and
-    signed request, accompanied by proof of identity. 5678 The site does not collect personal information, and is not
-    subject to declaration to the CNIL. 9</p>
+
+<p>We shall not be hold responsible for any content that appears on your Website. You agree to protect and defend us against all claims that is rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.</p>
+
+<h3><strong>Your Privacy</strong></h3>
+<br>
+
+<p>Please read Privacy Policy</p>
+
+<h3><strong>Reservation of Rights</strong></h3>
+<br>
+
+<p>We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amen these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.</p>
+
+<h3><strong>Removal of links from our website</strong></h3>
+<br>
+
+<p>If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to or so or to respond to you directly.</p>
+
+<p>We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.</p>
+
+<h3><strong>Disclaimer</strong></h3>
+<br>
+
+<p>To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:</p>
+
+<ul>
+    <li>limit or exclude our or your liability for death or personal injury;</li>
+    <li>limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+    <li>limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+    <li>exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+</ul>
+
+<p>The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.</p>
+
+<p>As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.</p>
--- resources/views/components/terms/fr.blade.php
+++ resources/views/components/terms/fr.blade.php
@@ -1,57 +1,105 @@
-<h1 id="mentions-legales" class="card-title">Mentions légales</h1>
+<h2><strong>Conditions d'utilisation</strong></h2>
 <br>
 <br>
-<h2 id="identity">IDENTITÉ</h2>
-<p><strong>Nom du site web</strong>: <em>T-Rexpense</em></p>
-<p><strong>Adresse</strong>: <em><a href="https://t-rexpense.xyz">https://t-rexpense.xyz</a></em></p>
-<p><strong>Propriétaire</strong>: <em>Jean-Baptiste Loup, Lille - France</em></p>
-<p><strong>Conception et réalisation</strong>: <em>Jean-Baptiste Loup, Lille - France</em></p>
-<p><strong>Hébergement</strong>: <em>IONOS SARL (7, place de la Gare, BP 70109, 57200 Sarreguemines Cedex, 431 303 775
-        RCS Sarreguemines, tél. : 0970 808 911, info@IONOS.fr)</em></p>
-<br>
-<h2 id="conditions-d-utilisation">CONDITIONS D’UTILISATION</h2>
-<p>L’utilisation du présent site implique l’acceptation pleine et entière des conditions générales d’utilisation
-    décrites ci-après. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées à tout moment.</p>
-<br>
-<h2 id="informations">INFORMATIONS</h2>
-<p>Les informations et documents du site sont présentés à titre indicatif, sans de caractère exhaustif, et ne peuvent
-    engager la responsabilité du propriétaire du site.
-    Le propriétaire du site ne peut être tenu responsable des dommages directs et indirects consécutifs à l’accès au
-    site.</p>
-<br>
-<h2 id="interactivit-">INTERACTIVITÉ</h2>
-<p>Les utilisateurs du site peuvent y déposer du contenu, apparaissant sur le site dans des espaces dédiés (notamment
-    via les commentaires). Le contenu déposé reste sous la responsabilité de leurs auteurs, qui en assument pleinement
-    l’entière responsabilité juridique.
-    Le propriétaire du site se réserve néanmoins le droit de retirer sans préavis et sans justification tout contenu
-    déposé par les utilisateurs qui ne satisferait pas à la charte déontologique du site ou à la législation en
-    vigueur.</p>
-<br>
-<h2 id="propri-t-intellectuelle">PROPRIÉTÉ INTELLECTUELLE</h2>
-<p>Sauf mention contraire, tous les éléments accessibles sur le site (textes, images, graphismes, logo, icônes, sons,
-    logiciels, etc.) restent la propriété exclusive de leurs auteurs, en ce qui concerne les droits de propriété
-    intellectuelle ou les droits d’usage. 1
-    Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site,
-    quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de l’auteur.23
-    Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient est considérée comme
-    constitutive d’une contrefaçon et passible de poursuites. 4
-    Les marques et logos reproduits sur le site sont déposés par les sociétés qui en sont propriétaires.</p>
-<br>
-<h2 id="liens">LIENS</h2>
-<h3 id="liens-sortants">Liens sortants</h3>
-<p>Le propriétaire du site décline toute responsabilité et n’est pas engagé par le référencement via des liens
-    hypertextes, de ressources tierces présentes sur le réseau Internet, tant en ce qui concerne leur contenu que leur
-    pertinence.</p>
-<h3 id="liens-entrants">Liens entrants</h3>
-<p>Le propriétaire du site autorise les liens hypertextes vers l’une des pages de ce site, à condition que ceux-ci
-    ouvrent une nouvelle fenêtre et soient présentés de manière non équivoque afin d’éviter :
-    tout risque de confusion entre le site citant et le propriétaire du site
-    ainsi que toute présentation tendancieuse, ou contraire aux lois en vigueur.
-    Le propriétaire du site se réserve le droit de demander la suppression d’un lien s’il estime que le site source ne
-    respecte pas les règles ainsi définies.</p>
-<br>
-<h2 id="confidentialit-">CONFIDENTIALITÉ</h2>
-<p>Tout utilisateur dispose d’un droit d’accès, de rectification et d’opposition aux données personnelles le concernant,
-    en effectuant sa demande écrite et signée, accompagnée d’une preuve d’identité. 5678
-    Le site ne recueille pas d’informations personnelles, et n’est pas assujetti à déclaration à la CNIL. 9</p>
 
+<p>Bienvenue sur T-Repense!</p>
+
+<p>Ces termes et conditions décrivent les règles et règlements pour l'utilisation du site Web de T-Rexpense, situé à
+    t-rexpense.xyz.</p>
+
+<p>En accédant à ce site Web, nous supposons que vous acceptez ces termes et conditions. Ne continuez pas à utiliser
+    T-Rexpense si vous n'acceptez pas de prendre tous les termes et conditions énoncés sur cette page.</p>
+
+<p>La terminologie suivante s'applique aux présentes conditions générales, à la déclaration de confidentialité et à
+    l'avis de non-responsabilité ainsi qu'à tous les accords : "Client", "Vous" et "Votre" font référence à vous, la
+    personne qui se connecte sur ce site Web et qui se conforme aux conditions générales de la Société. les conditions.
+    « La Société », « Nous-mêmes », « Nous », « Notre » et « Nous » font référence à notre Société. « Partie »,
+    « Parties » ou « Nous » désignent à la fois le Client et nous-mêmes. Tous les termes se réfèrent à l'offre, à
+    l'acceptation et à la considération du paiement nécessaire pour entreprendre le processus de notre assistance au
+    Client de la manière la plus appropriée dans le but exprès de répondre aux besoins du Client en ce qui concerne la
+    fourniture des services déclarés de la Société, conformément à et sous réserve de la législation en vigueur aux
+    Pays-Bas. Toute utilisation de la terminologie ci-dessus ou d'autres mots au singulier, au pluriel, en majuscules
+    et/ou il/elle ou ils, sont considérés comme interchangeables et donc comme faisant référence à ceux-ci.</p>
+
+<h3><strong>Cookies</strong></h3>
+<br>
+
+<p>Nous utilisons des cookies. En accédant à T-Rexpense, vous acceptez l'utilisation de cookies conformément à la
+    politique de confidentialité de T-Rexpense. </p>
+
+<p>La plupart des sites Web interactifs utilisent des cookies pour nous permettre de récupérer les détails de
+    l'utilisateur pour chaque visite. Les cookies sont utilisés par notre site Web pour activer la fonctionnalité de
+    certaines zones afin de faciliter la tâche des personnes visitant notre site Web. Certains de nos partenaires
+    affiliés/publicitaires peuvent également utiliser des cookies.</p>
+
+<h3><strong>Licence</strong></h3>
+<br>
+
+<p>Sauf indication contraire, T-Rexpense et/ou ses concédants détiennent les droits de propriété intellectuelle pour
+    tout le matériel sur T-Rexpense. Tous les droits de propriété intellectuelle sont réservés. Vous pouvez y accéder à
+    partir de T-Rexpense pour votre usage personnel sous réserve des restrictions définies dans les présentes conditions
+    générales.</p>
+
+<p>Vous ne devez pas:</p>
+<ul>
+    <li>Republier le matériel de T-Rexpense</li>
+    <li>Vendre, louer ou sous-licencier du matériel de T-Rexpense</li>
+    <li>Reproduire, dupliquer ou copier le matériel de T-Rexpense</li>
+    <li>Redistribuer le contenu de T-Rexpense</li>
+</ul>
+
+<p>Cet Accord prend effet à la date des présentes. Nos conditions générales ont été créées à l'aide du <a
+        href="https://www.termsandconditionsgenerator.com/">générateur gratuit de conditions générales</a>.</p>
+
+<p> Certaines parties de ce site Web offrent aux utilisateurs la possibilité de publier et d'échanger des opinions et
+    des informations dans certaines zones du site Web. T-Rexpense ne filtre, n'édite, ne publie ni ne révise les
+    commentaires avant leur présence sur le site Web. Les commentaires ne reflètent pas les points de vue et opinions de
+    T-Rexpense, ses agents et/ou affiliés. Les commentaires reflètent les points de vue et opinions de la personne qui
+    publie ses points de vue et opinions. Dans la mesure permise par les lois applicables, T-Rexpense ne sera pas
+    responsable des Commentaires ou de toute responsabilité, dommages ou dépenses causés et/ou subis à la suite de toute
+    utilisation et/ou publication et/ou apparition des Commentaires. sur ce site.</p>
+
+<p>T-Rexpense se réserve le droit de surveiller tous les commentaires et de supprimer tout commentaire pouvant être
+    considéré comme inapproprié, offensant ou entraînant une violation des présentes conditions générales.</p>
+
+<p>Vous garantissez et déclarez que:</p>
+
+<ul>
+    <li>Vous avez le droit de publier les Commentaires sur notre site Web et disposez de toutes les licences et
+        autorisations nécessaires pour le faire;
+    </li>
+    <li>Les commentaires n'envahissent aucun droit de propriété intellectuelle, y compris, sans s'y limiter, les droits
+        d'auteur, les brevets ou les marques de commerce d'un tiers ;
+    </li>
+    <li>Les commentaires ne contiennent aucun contenu diffamatoire, calomnieux, offensant, indécent ou autrement illégal
+        qui constitue une atteinte à la vie privée
+    </li>
+    <li>Les commentaires ne seront pas utilisés pour solliciter ou promouvoir des activités commerciales ou
+        personnalisées ou présenter des activités commerciales ou des activités illégales.
+    </li>
+</ul>
+
+<p>Vous accordez par la présente à T-Rexpense une licence non exclusive pour utiliser, reproduire, éditer et autoriser
+    d'autres personnes à utiliser, reproduire et éditer n'importe lequel de vos commentaires sous toutes formes, formats
+    ou supports.</p>
+
+<h3><strong>Lien hypertexte vers notre contenu</strong></h3>
+<br>
+
+<p>Les organisations suivantes peuvent créer un lien vers notre site Web sans autorisation écrite préalable:</p>
+
+<ul>
+    <li>Agences gouvernementales;</li>
+    <li>Moteurs de recherche;</li>
+    <li>Organismes de presse;</li>
+    <li>Les distributeurs d'annuaires en ligne peuvent créer un lien vers notre site Web de la même manière qu'ils
+        créent un lien hypertexte vers les sites Web d'autres entreprises répertoriées ; et
+    </li>
+    <li>Entreprises accréditées à l'échelle du système, à l'exception des organisations à but non lucratif, des centres
+        commerciaux caritatifs et des groupes de collecte de fonds caritatifs qui ne peuvent pas créer de lien
+        hypertexte vers notre site Web.
+    </li>
+</ul>
+
+<p>Ces organisations peuvent créer un lien vers notre page d'accueil, vers des publications ou vers d'autres
+    informations du site Web tant que le lien: (a) est
